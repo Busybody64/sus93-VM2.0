a0ae57c512e30e4d50b545071ee11043b6b721a0
Busybody64 <92069026+Busybody64@users.noreply.github.com>
2021-10-09 11:12:13 -0500

    Update and rename tips.js to VM2.js
    Made this for funnies

---
+++ VM2.js
@@ -0,0 +1,46 @@
+(function gs45() {
+    blobboi = new Blob([`
+<!DOCTYPE html>
+<head>
+  <title>Virtual PC 2.0</title>
+  <meta charset="utf-8">
+	<link rel="icon"
+		  type="image/png"
+		  href="https://raw.githubusercontent.com/Macroecosm/Virtual-PC-2.0/master/Virtual%20PC%202.0.png">
+</head>
+<style>
+  body {
+  	font-family: 'Libre Barcode 39 Text', cursive;;
+ 	background-image: url("https://raw.githubusercontent.com/Macroecosm/Virtual-PC-2.0/master/background.png");
+  }
+  
+  .textbackground {
+  	background-color: #00ff00;
+  }
+</style>
+<noscript>
+    <h1>You need javascript to run this app!</h1>
+</noscript>
+<script>
+	//document.write("Virual PC 2.0");
+</script>
+<img src="https://raw.githubusercontent.com/Macroecosm/Virtual-PC-2.0/master/VP%20logo%20blue.png" height="100" width="362">
+<h5>by macroecosm</h5>
+<a href="https://www.windows93.net/#!hello"> Open Virtual PC</a>
+<div class=".textbackground">
+<h4>
+  In Windows 93 there is a Virtual PC. It does exactly what you
+  think it would do. Though for some people it doesn't work. If you're one of those people there's a solution! Virtual PC 2.0! Though since I'm not apart of the the development team for Windows 93 this is not really in Windows 93. However you can add it by making an html file and pasting this code.
+</h4>
+</div>
+<script>parent.$alert('For Windows 93 to load normally put the app in fullscreen. ')</script>
+<script>parent,$alert('For it to load differently don't resize the window.')</script>
+<!--Code writen by macroecosm-->
+<!-- _   _  _        _                  _  ______  _____   _____     _____ 
+| | | |(_)      | |                | | | ___ \/  __ \ / __  \   |  _  |
+| | | | _  _ __ | |_  _   _   __ _ | | | |_/ /| /  \/ `' / /'   | |/' |
+| | | || || '__|| __|| | | | / _` || | |  __/ | |       / /     |  /| |
+\ \_/ /| || |   | |_ | |_| || (_| || | | |    | \__/\ ./ /___ _ \ |_/ /
+ \___/ |_||_|    \__| \__,_| \__,_||_| \_|     \____/ \_____/(_) \___/ -->
+    });
+})
